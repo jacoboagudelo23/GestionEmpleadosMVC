dae87a42c497481d96bc589e55e9c0670b5a6355
Jacobo Agudelo <jacoboagudelo3@gmail.com>
2026-05-12 17:06:40 -0500

    Proyecto ASP.NET Core MVC Gestion de Empleados

---
+++ Models/Empleado.cs
@@ -0,0 +1,23 @@
+﻿namespace GestionEmpleadosMVC.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Cargo { get; set; }
+
+        public DateTime FechaIngreso { get; set; }
+
+        public bool Activo { get; set; }
+
+        public int Antiguedad
+        {
+            get
+            {
+                return DateTime.Now.Year - FechaIngreso.Year;
+            }
+        }
+    }
+}
